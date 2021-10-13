81d5a2b7f4cf2bfed7874e911018c185abfb351d
Sanghwa Oh <minbosh92@gmail.com>
2021-10-13 14:37:50 +0900

    Update twoBits.py

--- programmers/twoBits.py
+++ programmers/twoBits.py
@@ -3,13 +3,16 @@ def solution(numbers):
     answer = []
     
     for i in range(len(numbers)) :
-        tmp = numbers[i] + 1
-        
-        while(1):
-            if( bin(numbers[i] ^ tmp).count('1') <= 2 ) :
-                answer.append(tmp)
-                break
-            else :
-                tmp += 1
-                
+        if(numbers[i] % 4 != 3) :
+            answer.append(numbers[i] + 1)
+        else :
+            tmp = bin(numbers[i])
+            tmp = "0" + tmp[2:]
+            
+            for i in range(len(tmp)-1,-1,-1) :
+                if(tmp[i] == "0") :
+                    tmp = tmp [0:i] + "10" + tmp[i+2:]
+                    break
+            answer.append(int(tmp,2))
+            
     return answer
