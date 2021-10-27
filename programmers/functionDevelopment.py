16db40984ead9517b2784bfb33b6c3eca5b5b2ea
Sanghwa Oh <minbosh92@gmail.com>
2021-10-27 16:03:20 +0900

    Create functionDevelopment.py

---
+++ programmers/functionDevelopment.py
@@ -0,0 +1,45 @@
+# 스택/큐 - 기능개발
+# 1번 풀이 O(n) = n^2
+def solution(progresses, speeds):
+    answer = []
+    
+    while len(progresses) > 0 :
+        for i in range(len(progresses)) :
+            if(progresses[i] < 100) :
+                progresses[i] += speeds[i]
+            else :
+                progresses[i] = 100
+        tmp = 0
+        for i in range(len(progresses)) :
+            if(progresses[i] >= 100) :
+                tmp += 1
+            else :
+                break
+        if(tmp > 0) :
+            progresses = progresses[tmp:]
+            speeds = speeds[tmp:]
+            answer.append(tmp)
+            
+    return answer
+  
+  # 2번 풀이 O(n) = n
+  def solution(progresses, speeds):
+    answer = []
+    day = []
+    
+    for i in range(len(progresses)) :
+        day.append(-((progresses[i] - 100) // speeds[i]))
+        
+    dDay = day[0]
+    task = 1
+    
+    for i in range(1,len(day)) :
+        if(dDay < day[i]) :
+            answer.append(task)
+            task = 1
+            dDay = day[i]
+        else :
+            task += 1
+    answer.append(task)
+            
+    return answer
