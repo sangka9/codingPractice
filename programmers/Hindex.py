60a489ccd4506592c59bfb56f6fbfc2f5db53b07
Sanghwa Oh <minbosh92@gmail.com>
2021-12-30 20:34:51 +0900

    Create Hindex.py

---
+++ programmers/Hindex.py
@@ -0,0 +1,11 @@
+# 코딩테스트 연습 - 정렬 - H-Index
+def solution(citations):
+    answer = 0
+    citations.sort()
+    
+    for i in range(len(citations)) :
+        if len(citations) - i <= citations[i] and i <= citations[i] :
+            answer = len(citations) - i
+            break
+    
+    return answer
