5d3c82ec3fc178379083202723b92363c3b28faf
Sanghwa Oh <minbosh92@gmail.com>
2022-08-03 12:18:14 +0900

    Create 01_sum-of-two-integers.py

---
+++ leetcode/01_sum-of-two-integers.py
@@ -0,0 +1,4 @@
+# binary - sum-of-two-integers - https://leetcode.com/problems/sum-of-two-integers/submissions/
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return sum([a,b])
