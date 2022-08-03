f58a0c32efc6daeab62ac16c637016f3bb63a21f
Sanghwa Oh <minbosh92@gmail.com>
2022-08-03 13:17:57 +0900

    Create 03_counting-bits.py

---
+++ leetcode/Binary/03_counting-bits.py
@@ -0,0 +1,24 @@
+# binary - counting-bits - https://leetcode.com/problems/counting-bits/submissions/
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        cnt = []
+        two = [2]
+        index = 0
+        
+        for i in range(1,16) :
+            two.append(two[i-1]*2)
+        
+        for i in range(n+1) :
+            if i == 0 :
+                cnt.append(0)
+            elif i == 1 :
+                cnt.append(1)
+            elif i > 1 :
+                if i in two : # 2의 제곱수 확인
+                    index = 1
+                    cnt.append(1)
+                else :
+                    cnt.append(cnt[index] + 1)
+                    index += 1
+        
+        return cnt
