41092081607bc24015e8517d34624f751e1ec1a6
Sanghwa Oh <minbosh92@gmail.com>
2023-02-03 15:41:46 +0900

    Update Q3.py

--- kt_aivle/230203/Q3.py
+++ kt_aivle/230203/Q3.py
@@ -15,3 +15,29 @@ store = [2,3,7,8,9]
 customer = [7,5,9]
 answer = solution(store, customer)
 print(answer)
+
+##########################################
+
+# Q3 answer template
+
+def solution(store, customer):
+    answer = []
+    clist = [0 for i in range(101)]
+    slist = [0 for i in range(101)]
+    
+    for i in range(len(store)) : slist[store[i]] = 1
+        
+    for i in range(len(customer)) : clist[customer[i]] = 1
+    
+    for i in range(len(customer)) :
+        if clist[customer[i]] and slist[customer[i]] :
+            answer.append('yes')
+        else :
+            answer.append('no')
+    
+    return answer
+
+store = [2,3,7,8,9]
+customer = [7,5,9]
+answer = solution(store, customer)
+print(answer)
