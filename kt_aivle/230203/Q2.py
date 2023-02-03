1f79c37eb6bad1159a870d8ed2baa2256eda5137
Sanghwa Oh <minbosh92@gmail.com>
2023-02-03 15:10:11 +0900

    Create Q2.py

---
+++ kt_aivle/230203/Q2.py
@@ -0,0 +1,14 @@
+# Q2 Answer template
+
+def solution(numbers):
+    answer = 0
+    
+    for i in range(10) :
+        if i not in numbers :
+            answer += i
+    
+    return answer
+
+numbers = [1,2,3,4,6,7,8,0]
+answer = solution(numbers)
+print(answer)
