026c8a6622ae3de17b12f8a0b38328a010c8a908
Sanghwa Oh <minbosh92@gmail.com>
2023-02-03 15:34:11 +0900

    Update Q2.py

--- kt_aivle/230203/Q2.py
+++ kt_aivle/230203/Q2.py
@@ -12,3 +12,15 @@ def solution(numbers):
 numbers = [1,2,3,4,6,7,8,0]
 answer = solution(numbers)
 print(answer)
+
+#####################################
+
+# Q2 Answer template
+
+def solution(numbers):
+    answer = 45 - sum(numbers)
+    return answer
+
+numbers = [1,2,3,4,6,7,8,0]
+answer = solution(numbers)
+print(answer)
